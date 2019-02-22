206f4fdc9945714adbf714930fe5db7b94b94b9a
sayi <adamokuns@gmail.com>
2019-02-21 20:30:27 -0500

    Adjusted syntax to includer new user downcase loop, still gives error

--- if_else/check_user.py
+++ if_else/check_user.py
@@ -5,6 +5,8 @@ current_users = ['esosa', 'eromose', 'ayobami','onos', 'chikezie']
 new_users = ['matt', 'Esosa', 'harnor', 'suyi', 'sadiq']
 	
 for current_user in current_users:
+	for new_user in new_users:
+		new_user.lower()
 	if current_user.lower() in new_users:
 		print("Sorry " + current_user + " is already taken.")
 	else:
